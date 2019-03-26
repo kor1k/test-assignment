611ad0d6dbe556ba6662ca0beddd349ee3296434
kor1k <koortttt@gmail.com>
2019-03-26 12:23:48 +0200

    colored variants on click

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,33 @@ $(document).ready(function () {
         $('#hide').hide()
     });
 });
+
+$(document).ready(function () {
+    $('.choose__variant').click(function () {
+        $(this).css('background', '#fc8950');
+        $(this).css('color', 'white');
+        $(this).toggleClass('choose__variant__next-section');
+        // $('.background__section').css('background', '#fc8950');
+        // $('.background__section').css('opacity', '0.2');
+    });
+});
+
+$(document).ready(function () {
+    $('.choose__variant').click(function () {
+        $('.background__section').toggleClass('background__section__test-color');
+    });
+});
+
+// if (document.getElementsByName('choose__variant'))
+
+// $(document).ready(function () {
+//     $("#variant-1").click(function () {
+//         $('a').css({
+//             'background-color: black'
+//         });
+//     })
+// })
+
+// $('.choose__variant .variant-1').click(function(){
+//     $(this).toggleClass('clicked-button');
+// });

8281bde5483c07a44f2312b90a02653323423061
kor1k <koortttt@gmail.com>
2019-03-26 13:18:53 +0200

    broke reg from and actions too

--- src/js/script.js
+++ src/js/script.js
@@ -8,26 +8,65 @@
 
 $(document).ready(function () {
     $("#start").click(function () {
+        $('#reg-form-section').hide();
         $("div").show();
-        $('#hide').hide()
+        $('#main-page').hide();
     });
 });
+
+// $(document).ready(function () {
+//     $('#continue-to-next-block').click(function () {
+//         $('#reg-form-section').css('display','flex')
+//     })
+// })
+
+$(document).ready(function () {
+    $("#continue-to-next-block").click(function () {
+        $('#main-page').hide();
+        $('#first-section').hide();
+        $("#reg-form-section").show();
+        // $('#main-page').hide();
+    });
+});
+
 
 $(document).ready(function () {
     $('.choose__variant').click(function () {
         $(this).css('background', '#fc8950');
         $(this).css('color', 'white');
-        $(this).toggleClass('choose__variant__next-section');
+        $('.choose__variant__next-section').css('display', 'flex');
+        // $('.test_percent_and_choosed-variant-text').css('display', 'flex')
+        // $(this).toggleClass('choose__variant__next-section');
         // $('.background__section').css('background', '#fc8950');
         // $('.background__section').css('opacity', '0.2');
     });
 });
 
-$(document).ready(function () {
-    $('.choose__variant').click(function () {
-        $('.background__section').toggleClass('background__section__test-color');
-    });
-});
+// $(document).ready(function () {
+//     $('#variant-1').click(function () {
+// $('.background__section').toggleClass('.test_percent_and_choosed-variant-text_a')
+// })
+// })
+
+// (function () {
+//     $('#variant-2').click(function () {
+//         $('.test_percent_and_choosed-variant-text_b').css('display', 'flex')
+//     })
+// })
+// (function () {
+//     $('#variant-3').click(function () {
+//         $('.test_percent_and_choosed-variant-text_v').css('display', 'flex')
+//     })
+// })
+
+
+// $('.choose__variant__next-section').show(1500);
+
+// $(document).ready(function () {
+//     $('.choose__variant').click(function () {
+//         $('.background__section').toggleClass('background__section__test-color');
+//     });
+// });
 
 // if (document.getElementsByName('choose__variant'))
 

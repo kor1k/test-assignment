50c229d2cb28569732b9304372cb2c0698e8b22b
kor1k <koortttt@gmail.com>
2019-03-28 11:06:38 +0200

    added text in options on click btn

--- src/js/script.js
+++ src/js/script.js
@@ -15,12 +15,50 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('.content-side-options__option').click(function (e) {
+    $('.content-side-options__option').click(function () {
         $('.content-side-options__option').removeClass('active');
         $(this).toggleClass('active');
-        // $(this).toggleClass()
     })
 });
+
+$('.content-side-options__option').click(function (e) {
+    e.preventDefault();
+    $('.content-side-options__option').removeClass('active');
+    $(event.target).addClass('active');
+    $('.content-side-options__button-next-content-side').addClass('active');
+    $('.content-side-main--result').addClass('active');
+    $('.content-side-main-result__view-percent').text($(event.target).attr('data-perc'));
+    $('.content-side-main-result__view-text').text('Людей обращает внимание на ' + $(event.target).attr('data-letter'));
+});
+
+// click on option_1 -> active - op1
+// click on option_2 -> removeActive from op1 and make active op2
+// $(document).ready(function () {
+//     $('.option').click(function () {
+//         $('.test-alert_1').toggleClass('active');
+//         $('.test-alert_1').removeClass('active');
+//     })
+// });
+//
+// $(document).ready(function () {
+//     $('.option').click(function () {
+//         $('.test-alert_2').toggleClass('active');
+//         $(this).removeClass('active');
+//     })
+// });
+//
+// $(document).ready(function () {
+//     $('.option').click(function () {
+//         $('.test-alert_3').toggleClass('active');
+//         $(this).removeClass('active');
+//     })
+// });
+
+
+$('.content-side-options__button-next-content-side').click(function () {
+    $('.content-side-main-result').css('display', 'none')
+})
+
 
 $(document).ready(function () {
     $('.content-side-form__choose-sex__male, .content-side-form__choose-sex__female').click(function (e) {

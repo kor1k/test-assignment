e29a31b838962f46c384865d18bf893289821c65
kor1k <koortttt@gmail.com>
2019-03-29 19:00:14 +0200

    fixed footer and added some regexp patterns in input forms

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,8 @@ $(document).ready(function () {
     $('#start').click(function () {
         $('.content-side-main').toggleClass('not-active');
         $('.content-side-options').toggleClass('active');
-        $('.elementToAddAnimationTo').addClass('animation');
         $('.footer').animate({
-            width: '62%',
+            width: '62.2%',
         }, 500);
         $('.footer__menu').animate({
             width: '80%',
@@ -15,7 +14,6 @@ $(document).ready(function () {
         $('.content-side-options').toggleClass('not-active');
         $('.content-side-form').toggleClass('active');
         $('.footer').toggleClass('active_forms');
-        $('.elementToAddAnimationTo').addClass('animation');
     });
 
     $('.content-side-options__option').click(function () {
@@ -48,44 +46,14 @@ $(document).ready(function () {
     });
 });
 
-// $('#reg-form-submit').click(function () {
-//
-//     $('#modal').addClass('open');
-//     $('.overlay').addClass('show');
-//     // $("#form_phone").val('');
-//     // $("#form_email").val('');
-//     // $("#form_email").val('');
-//
-//     $('.close, .overlay').click(function () {
-//         $('.overlay').removeClass('show');
-//         $('#modal').removeClass('open');
-//     });
-// });
-
 // function validationPatterns() {
 //     let nameFlag = /^([a-zA-Z]{3,16})$/;
 //     let emailFlag = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 //     let passwordFlag = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-//     // let passwordFlag =
 // }
 
-// $('#form-fields__inputs > input').click(function () {
-//     let required = $('input').filter('[required]:visible');
-//     let allRequired = true;
-//     required.each(function () {
-//         if ($(this).val() === '') {
-//             allRequired = false;
-//         } else {
-//             $('content-side-form__form-submit-btn').toggleClass('active');
-//         }
-//     });
-//     if (!allRequired) {
-//         $('.req-checker').toggleClass('active');
-//     }
-// });
-
 $('#form-fields__inputs input').on('click keyup', function () {
-    console.log($('#form_checkbox').is(':checked'));
+        console.log($('#form_checkbox').is(':checked'));
         if ($('#form_name').val() !== "" && $('#form_email').val() !== "" && $('#form_checkbox').is(':checked') == true && $('#form_pass').val().length >= 6) {
             console.log($('#form_checkbox').is(':checked'));
             $('.content-side-form__form-submit-btn').addClass('active');
